981075afa370bfeb67a8974c36842fa9ca1e9087
Milad Nekofar <milad@nekofar.com>
2025-03-29 07:27:12 +0300

    chore(deps-dev): update `eslint-plugin-jsdoc` config
    switch to `flat/recommended-typescript` for improved compatibility
    and to align with updated recommendations.

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,7 @@ export default typescriptEslint.config(
     ignores: ['**/build/**', '**/dist/**', '*.config.{js,mjs,ts}'],
   },
   javascriptEslint.configs.recommended,
-  eslintPluginJsdoc.configs['flat/recommended-typescript-error'],
+  eslintPluginJsdoc.configs['flat/recommended-typescript'],
   eslintPluginRegexp.configs['flat/recommended'],
   ...typescriptEslint.configs.strictTypeChecked,
   ...typescriptEslint.configs.stylisticTypeChecked,
