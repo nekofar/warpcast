9274e5aeb31f3d85bc8de78e4dbc7bea5c65a7f6
Milad Nekofar <milad@nekofar.com>
2025-09-17 19:08:41 +0300

    refactor(config): disable `splitting` in tsup config
    ensured consistent module output in bundled files by disabling `splitting`, simplifying dependency management and improving compatibility.

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     return { js: format === 'esm' ? '.mjs' : '.cjs' }
   },
   bundle: true,
+  splitting: false,
   external: ['zod'], // keep zod as a peer dependency, do not bundle
   noExternal: [],
 })
