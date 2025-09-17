28b620878e918365abdd8edcd49a42bf664695c1
Milad Nekofar <milad@nekofar.com>
2025-09-17 19:08:32 +0300

    refactor(config): enable `clean` option in build process
    ensured the output directory is cleaned before each build to prevent stale artifacts and maintain a consistent build environment.

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ['src/**/*.ts', '!src/**/*.test.ts'],
   format: ['esm', 'cjs'],
   dts: true,
+  clean: true,
   outDir: 'dist',
   outExtension({ format }) {
     return { js: format === 'esm' ? '.mjs' : '.cjs' }
